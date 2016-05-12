7272a618e3ea62a1905bf2a8cc53b0d967e5fa2c
Willem de Groot <willem@byte.nl>
2016-05-12 17:36:48 +0200

    Deploy fixes

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,5 @@
+DEPLOY_DIR="$(dirname "$0")"
 host=178.62.221.43
 tools/md_to_html.sh
-rsync --exclude=.git --exclude=magerundumps/ --exclude=postdata/ --exclude=*.pyc --exclude=.idea -va ~/code/modules.hypernode.com/ root@$host:/srv/app/
-#rsync --exclude=.git --exclude=postdata/ --exclude=*.pyc --delete --exclude=.idea -va ~/code/modules.hypernode.com/ root@$host:/srv/magmodules/
+rsync --exclude=.git --exclude-from=.gitignore --exclude=magerundumps/ --exclude=postdata/ --exclude=*.pyc --exclude=.idea -va $DEPLOY_DIR/../ root@$host:/srv/app/
 ssh root@$host service uwsgi restart
